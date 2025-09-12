0d8887f56ef29e2c740992705c822a475ad765cb
cajaun <cajaun.campbell@gmail.com>
2025-09-12 16:19:44 -0500

    fixing the border issues

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,7 +3,7 @@ import ThemeToggleButton from '@/components/hooks/useTheme'
 
 const Footer = () => {
   return (
-    <footer className="mx-auto mt-auto w-full max-w-screen-sm border-t-[0.5px] border-[#EBEBEB] dark:border-[#2C2C2B] px-4 ">
+    <footer className="mx-auto mt-auto w-full max-w-screen-sm border-t border-[#EBEBEB] dark:border-[#2C2C2B] px-4 ">
     <div
       className="flex items-center justify-between px-0 pt-4 md:px-0"
       style={{ paddingBottom: "max(16px, env(safe-area-inset-bottom))" }}
